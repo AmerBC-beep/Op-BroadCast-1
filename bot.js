7528e47a4271658bf7b5377a9c3b724fa305425e
AmerBC-beep <57823686+AmerBC-beep@users.noreply.github.com>
2019-11-16 19:57:31 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,12 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "1"
-var adminprefix = '1'
+var prefix = "3"
+var adminprefix = '3'
 
  //broadcast
 
 client.on("message", message => {
-    if (message.content.startsWith("1bc")) {
+    if (message.content.startsWith("3bc")) {
                  if (!message.member.hasPermission("ADMINISTRATOR"))  return;
   let args = message.content.split(" ").slice(1);
   var argresult = args.join(' ');
@@ -21,7 +21,7 @@ client.on("message", message => {
 
  //online broadcast
 
- var prefix = "1";
+ var prefix = "3";
 
   client.on("message", message => {
   
